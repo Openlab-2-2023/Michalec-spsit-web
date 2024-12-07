bb078e58898cd24107cf53a6a0d79547fe1193e8
martin-nb995 <Admin@0z6sw.onmicrosoft.com>
2024-12-07 16:14:56 +0100

    changed background

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,100 @@
+import * as THREE from 'https://cdn.skypack.dev/three@v0.122.0';
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function rgb(r, g, b) {
+  return new THREE.Vector3(r, g, b);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  // Background stuff
+  // https://codepen.io/smpnjn/pen/pobGMKp
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // document.body.appendChild(renderer.domElement)
+  document.body.insertBefore(renderer.domElement, document.body.firstChild);
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  let vCheck = false;
+
+  camera.position.z = 5;
+
+  var randomisePosition = new THREE.Vector2(1, 2);
+
+  var R = function (x, y, t) {
+    return (Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + t)));
+  }
+
+  var G = function (x, y, t) {
+    return (Math.floor(192 + 64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)));
+  }
+
+  var B = function (x, y, t) {
+    return (Math.floor(192 + 64 * Math.sin(5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)));
+  }
+  let sNoise = document.querySelector('#snoise-function').textContent
+  let geometry = new THREE.PlaneGeometry(window.innerWidth / 2, 400, 100, 100);
+  let material = new THREE.ShaderMaterial({
+    uniforms: {
+      u_bg: { type: 'v3', value: rgb(208, 38, 38) }, // red
+      u_bgMain: { type: 'v3', value: rgb(22, 144, 16) }, // darker green
+      u_color1: { type: 'v3', value: rgb(58, 175, 223) }, // light blue
+      u_color2: { type: 'v3', value: rgb(170, 199, 14) }, // yellow
+      u_time: { type: 'f', value: 30 },
+      u_randomisePosition: { type: 'v2', value: randomisePosition }
+    },
+    fragmentShader: sNoise + document.querySelector('#fragment-shader').textContent,
+    vertexShader: sNoise + document.querySelector('#vertex-shader').textContent,
+  });
+
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(-200, 270, -280);
+  mesh.scale.multiplyScalar(4);
+  mesh.rotationX = -1.0;
+  mesh.rotationY = 0.0;
+  mesh.rotationZ = 0.1;
+  scene.add(mesh);
+
+  renderer.render(scene, camera);
+  let t = 0;
+  let j = 0;
+  let x = randomInteger(0, 32);
+  let y = randomInteger(0, 32);
+  const animate = function () {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    mesh.material.uniforms.u_randomisePosition.value = new THREE.Vector2(j, j);
+
+    mesh.material.uniforms.u_color1.value = new THREE.Vector3(R(x, y, t / 2), G(x, y, t / 2), B(x, y, t / 2));
+
+    mesh.material.uniforms.u_time.value = t;
+    if (t % 0.1 == 0) {
+      if (vCheck == false) {
+        x -= 1;
+        if (x <= 0) {
+          vCheck = true;
+        }
+      } else {
+        x += 1;
+        if (x >= 32) {
+          vCheck = false;
+        }
+
+      }
+    }
+
+    // Increase t by a certain value every frame
+    j = j + 0.01;
+    t = t + 0.05;
+  };
+  animate();
+
+  // Background stuff
+
   const top_pagebar = document.querySelectorAll(".top-pagebar a");
   const pages = document.querySelectorAll('.page > div[id^="page-"]');
   const left_sidebar = document.querySelectorAll(
@@ -12,7 +108,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
-    toggleSidebar();
+    //toggleSidebar();
   });
 
   // Function to toggle sidebar visibility
@@ -57,7 +153,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   left_sidebar.forEach((link) => {
     link.addEventListener("click", function (event) {
-      if (this.hasAttribute("href")) {
+      if (this.hasAttribute("href")) { // firefox opening tabs twice
         // Open the link in a new tab
         window.open(this.getAttribute("href"), "_blank");
       } else {
